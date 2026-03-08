77cf71ad987ee0af51dc883b05091760622974a8
Garik <garikgevorgyan.rap@mail.ru>
2026-03-08 14:15:03 +0400

    refactor: remove multiple CSS files for AdvertisingCabinet, BackButton, CashInForm, CashInRequestForm, CashOutForm, FootMenu, Friends, MinerAnimation, ProfileMenu, and TransactionTable components to streamline styling and improve maintainability

--- src/components/BackButton.jsx
+++
@@ -1,9 +0,0 @@
-import './BackButton.css'
-
-export default function BackButton( { setCurrentContent, path } ) {
-    return (
-        <div className="back-button" onClick={() => setCurrentContent(path)}>
-            <img src="/assets/back.svg" alt="back" />
-        </div>
-    )
-}
