0b2ee846019313c4d871d5bfe3dd5d57a637cbc0
Garik <garikgevorgyan.rap@mail.ru>
2026-03-08 16:04:03 +0400

    refactor: update i18n configuration to improve language loading and maintainability

--- src/i18n.js
+++ src/i18n.js
@@ -1,17 +1,17 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
 
 i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'RU',
+    fallbackLng: "RU",
     debug: false,
     interpolation: { escapeValue: false },
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json'
-    }
+      loadPath: "/locales/{{lng}}/{{ns}}.json?v=1.0.1",
+    },
   });
 
 export default i18n;
