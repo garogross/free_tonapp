cd94dc8ac31ac2e436758db0b4e9c73388152e2c
Garik <garikgevorgyan.rap@mail.ru>
2026-03-10 16:48:22 +0400

    refactor: change amount format to string in MiningSpeedInfoModal for consistency

--- src/components/MiningSpeedInfoModal/MiningSpeedInfoModal.jsx
+++ src/components/MiningSpeedInfoModal/MiningSpeedInfoModal.jsx
@@ -245,7 +245,7 @@ const MiningSpeedInfoModal = ({
               <>
                 <h3 className={styles.miningSpeedInfoModal__title}>
                   {t("acceleratorsBlocked.friendsInfo", {
-                    amount: 0.00000024,
+                    amount: "0.00000024",
                     mode: "stars",
                   })}
                 </h3>
